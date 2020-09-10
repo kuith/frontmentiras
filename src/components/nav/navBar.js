c188e104361a8180e962928f7472e24296baae72
Rafa <kuithsteam@gmail.com>
2020-09-10 22:48:16 +0200

    Solucionado CORS

--- src/components/nav/navBar.js
+++ src/components/nav/navBar.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link } from "react-router-dom";
 
 import {
-  jornadas ,
+  //jornadas ,
   investigadores,
 } from '../../util/dataUtil/datos'; // recuperar de la base de datos
 
@@ -14,33 +14,51 @@ const NavBar = () => {
   const [jornadasDatos, setJornadasDatos] = useState([]);
   const [investigadoresDatos, setInvestigadoresDatos] = useState([]);
 
+  //'http://localhost:3001/api/jornada', modificado para intentar solucionar lo del cors
+  //en el archivo package.json he añadido la linea /* "proxy": "http://localhost:3001/api" */
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        'http://localhost:3001/api/jornada',
-      );
+      const result = await axios.get("/jornada");
 
-      setJornadasDatos(result.data);
-      console.log(jornadasDatos);
+      setJornadasDatos(result.data.jornadas);
+      console.log(result.data.jornadas);
     };
-      fetchData();
+    fetchData();
   }, []);
-  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/investigador");
+
+      setInvestigadoresDatos(result.data.investigadores);
+      console.log(console.log(result.data.investigadores));
+    };
+    fetchData();
+  }, []);
+
   //jornadas
-  const jornadasNavItem = jornadasDatos.map((jornada) => (
-    <Link className="dropdown-item"
-      to={`/jornadas/${jornada._id}`}> 
-      {jornada.nombre}
+  const jornadasNavItem = jornadasDatos.map(jornada => (
+    <Link className="dropdown-item" to={`/jornadas/${jornada._id}`}>
+      {jornada.nombreJornada}
     </Link>
   ));
   const jornadasNav = (
     <li className="nav-item dropdown ">
-      <Link className="nav-link dropdown-toggle text-dark"
+      <Link
+        className="nav-link dropdown-toggle text-dark"
         to="#"
-        id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        id="navbarDropdownMenuLink"
+        role="button"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
         Jornadas
       </Link>
-      <div className="dropdown-menu elementoLista" aria-labelledby="navbarDropdownMenuLink">
+      <div
+        className="dropdown-menu elementoLista"
+        aria-labelledby="navbarDropdownMenuLink"
+      >
         {jornadasNavItem}
         <Link to="/jornadas/nuevaJornada" className="dropdown-item">
           Nueva Jornada
@@ -50,20 +68,28 @@ const NavBar = () => {
   );
 
   //investigadores
-  const investigadoresNavItem = investigadoresDatos.map((investigador) => (
-    <Link className="dropdown-item"
-      to={`/investigadores/${investigador._id}`}> 
+  const investigadoresNavItem = investigadoresDatos.map(investigador => (
+    <Link className="dropdown-item" to={`/investigadores/${investigador._id}`}>
       {investigador.nombre}
     </Link>
   ));
   const investigadoresNav = (
     <li className="nav-item dropdown ">
-      <Link className="nav-link dropdown-toggle text-dark"
+      <Link
+        className="nav-link dropdown-toggle text-dark"
         to="#"
-        id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        id="navbarDropdownMenuLink"
+        role="button"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
         Investigadores
       </Link>
-      <div className="dropdown-menu elementoLista" aria-labelledby="navbarDropdownMenuLink">
+      <div
+        className="dropdown-menu elementoLista"
+        aria-labelledby="navbarDropdownMenuLink"
+      >
         {investigadoresNavItem}
       </div>
     </li>
@@ -72,14 +98,21 @@ const NavBar = () => {
   //Navbar
   const navBarTotal = (
     <nav className="navbar navbar-expand-sm ">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon bg-warning text-dark"></span>
       </button>
       <div className="collapse navbar-collapse " id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <Link className="nav-link text-dark"
-              to="/">
+            <Link className="nav-link text-dark" to="/">
               Inicio <span className="sr-only">(current)</span>
             </Link>
           </li>
@@ -91,11 +124,11 @@ const NavBar = () => {
   );
 
   return (
-    <div className = "barraNavegacion">
+    <div className="barraNavegacion">
       {navBarTotal}
-      <hr/>
+      <hr />
     </div>
-  )
+  );
 }
 
 export default NavBar;
